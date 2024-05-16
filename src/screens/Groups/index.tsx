61ead4b7e1c4b8867751b4e62f5cfc5fb7c72d26
Gabriel TJRO <gabriel.mattos@tjro.jus.br>
2024-05-16 11:38:52 -0400

    Adicionado o tema a aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,26 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
-
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
-        <View style={styles.container}>
-            <Text style={styles.text}>Groups</Text>
-        </View>
+
     </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#FFF',
-    fontSize: 32
-  }
-});

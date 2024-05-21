ad64624db50b43b372f2da343657fb917f1651c9
Gabe-Home <gmattoslopes@gmail.com>
2024-05-21 19:57:54 -0400

    adicionado input e botão de adicionar na tela de players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -2,7 +2,8 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
+import { Input } from "@components/Input";
 
 export function Players() {
   return (
@@ -13,8 +14,15 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
-
-      <ButtonIcon />
+      <Form>
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon 
+          icon="add"
+        />
+      </Form>
     </Container>
   )
 }
